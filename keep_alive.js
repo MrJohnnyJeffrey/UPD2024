3014a7e0cf80c39dd3167c9a604dc4809074adb3
MrJohnnyJeffrey <thuyphung.vntas@gmail.com>
2024-06-18 17:13:03 +0700

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,1 +1,15 @@
+const express = require("express")
 
+const server = express()
+
+server.all("/", (req, res) => {
+  res.send("NAHHHHH")
+})
+
+function keepAlive() {
+  server.listen(8080, () => {
+    console.log("Server is up and running, Make it 24/7 now!")
+  })
+}
+
+module.exports = keepAlive
